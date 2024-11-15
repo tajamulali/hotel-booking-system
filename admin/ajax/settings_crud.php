812467c611555690f86dd3bde9284ed860a1a888
tajamulali <nuwaib14@gmail.com>
2024-11-15 18:00:50 +0530

    updated adam page

--- admin/ajax/settings_crud.php
+++ admin/ajax/settings_crud.php
@@ -24,4 +24,14 @@
         echo $res;
     }
 
+    if (isset($_POST['upd_shutdown']))
+    {
+        $frm_data = ($_POST['upd_shutdown']==0) ? 1 : 0;
+
+        $q = "UPDATE `settings` SET `shutdown`=? WHERE `s_no`=?";
+        $values = [$frm_data,1];
+        $res = update($q, $values,'ii');
+        echo $res;
+    }
+
 ?>
--- admin/settings.php
+++ admin/settings.php
@@ -1,4 +1,5 @@
 <?php
+    require('inc/script.php');
     require('inc/essentials.php');
     adminLogin();
     session_regenerate_id(true);
@@ -23,8 +24,8 @@
 
             <!-- General settings section -->
 
-            <div class="card">
-                <div class="card-body">
+            <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body border-0 shadow-sm">
                     <div class="d-flex align-items-center justify-content-between mb-3">
                         <h5 class="card-title m-0">General Settings</h5>
                         <button type="button" class="btn btn-dark shadow-none btn-sm" data-bs-toggle="modal" data-bs-target="#general-s">
@@ -39,40 +40,56 @@
             </div>
 
            
-                <!-- General settings Modal -->
-                <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <form>
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">General Settings</h5>
+            <!-- General settings Modal -->
+            <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <form>
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">General Settings</h5>
+                            </div>
+                            <div class="modal-body">
+                                <div class="mb-3">
+                                    <label class="form-label">Site Title</label>
+                                    <input type="text" name="site_title" id="site_title_inp" class="form-control shadow-none">
                                 </div>
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label class="form-label">Site Title</label>
-                                        <input type="text" name="site_title" id="site_title_inp" class="form-control shadow-none">
-                                    </div>
-                                    <div class="mb-3">
-										<label class="form-label">About Us</label>
-										<textarea name="site_about" id="site_about_inp" class="form-control shadow-none" rows="6"></textarea>
-									</div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" onclick="site_title.value = general_data.site_title, site_about.value = general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
-                                    <button type="button" onclick="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">Submit</button>
+                                <div class="mb-3">
+                                    <label class="form-label">About Us</label>
+                                    <textarea name="site_about" id="site_about_inp" class="form-control shadow-none" rows="6"></textarea>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                            <div class="modal-footer">
+                                <button type="button" onclick="site_title.value = general_data.site_title, site_about.value = general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
+                                <button type="button" onclick="upd_general(site_title.value, site_about.value)" class="btn custom-bg text-white shadow-none">Submit</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
+
+            <!-- Shutdown section -->
+           <div class="card border-0 shadow-sm">
+                <div class="card-body border-0 shadow-sm">
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <h5 class="card-title m-0">Shutdown Website</h5>
+                        <div class="form-check form-switch">
+                            <form>
+                                <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox" id="shutdown-toggle">
+                            </form>
+                        </div>
+                    </div>
+                    <p class="card-text" id="site_about">
+                        No customers will be allowed to book room, when shutdown mode is turned on.
+                    </p>
+                </div>
             </div>
+
         </div>
     </div>
 </div>
+            
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<?php require('inc/script.php'); ?>
 <script>
     let general_data;
     function get_general()
@@ -82,6 +99,8 @@
 
         let site_title_inp = document.getElementById('site_title_inp');
         let site_about_inp = document.getElementById('site_about_inp');
+
+        let shutdown_toggle = document.getElementById('shutdown-toggle');
 
         let xhr = new XMLHttpRequest();
         xhr.open("POST","ajax/settings_crud.php",true);
@@ -95,6 +114,15 @@
 
             site_title_inp.value =general_data.site_title;
             site_about_inp.value =general_data.site_about;
+
+            if(general_data.shutdown == 0){
+                shutdown_toggle.checked = false;
+                shutdown_toggle.value = 0;
+            }
+            else{
+                shutdown_toggle.checked = true;
+                shutdown_toggle.value = 1;
+            }
         }
 
         xhr.send('get_general');
@@ -112,18 +140,41 @@
             var modal = bootstrap.Modal.getInstance(myModal);
             modal.hide();
             
-            if(this.responseText == 1){
-                console.log('data updated');
+            if(this.responseText == 1)
+            {
+                alert('success','Changes saved !');
                 get_general();
             }
-            else{
-                console.log('no changes made');
+            else
+            {
+                alert('error','No changes made');
             }
         }
 
         xhr.send('site_title='+site_title_val+'&site_about='+site_about_val+'&upd_general');
     }
 
+    function upd_shutdown(val){
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST","ajax/settings_crud.php",true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload = function(){
+            
+            if(this.responseText == 1 && general_data.shutdown==0)
+            {
+                alert('success','Site has been shutdown!');
+               
+            }
+            else
+            {
+                alert('success','shutdown mode off!');
+            }
+            get_general();
+        }
+
+        xhr.send('upd_shutdown='+val);
+    }
 
     window.onload = function(){
         get_general();
