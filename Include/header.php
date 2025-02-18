d0e3d69b18a160cca00d0f414407a667fa43e3e5
tajamulali <nuwaib14@gmail.com>
2025-02-18 14:27:04 -0800

    initial

--- Include/header.php
+++ Include/header.php
@@ -13,7 +13,7 @@
 
 <nav id="nav-bar" class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-5 sticky-top">
   <div class="container-fluid">
-		<a class="navbar-brand me-5 fw=bold fs-3 h-font" href="index.php">SMA HOTEL</a>
+		<a class="navbar-brand me-5 fw=bold fs-3 h-font" href="index.php">WaibyHotel HOTEL</a>
       <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			 <span class="navbar-toggler-icon"></span>
 					</button>
